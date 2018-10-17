28bca63676f274477c246d65755940fb36fa8ebc
mbp3 <mbp3@pct.edu>
2018-10-17 15:46:51 -0400

    navbar should be fully functional with bootstrap 4

--- SignRequestExpress/Controllers/HomeController.cs
+++ SignRequestExpress/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace SignRequestExpress.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
